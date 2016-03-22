3088d29e641ee1b3252e14614025a52243181f87
Xiaoguang Huo <xhuo2@L-SB835ULG8W-M.local>
2016-03-21 18:06:25 -0700

    update rm.js

--- rm.js
+++ rm.js
@@ -7,29 +7,42 @@ let argv = process.argv
 let co = require('co')
 
 
-function* rm() {
-    let fileName =  argv[2]
-    let stats = yield fs.stat(fileName)
-    if(!stats.isDirectory()) {
-        yield fs.unlink(fileName)
-    }else{
-        rmall(fileName)
-    }
+function* main() {
+    rm(argv[2])
+    //let fileName =  argv[2]
+    //let stats = yield fs.stat(fileName)
+    //if(!stats.isDirectory()) {
+    //    yield fs.unlink(fileName)
+    //}else{
+    //    rmall(fileName)
+    //}
 }
 
-let rmall = co.wrap(function* (rootPath){
-    let fileNames = yield fs.readdir(rootPath)
-
-    for (let fileName of fileNames){
-        let curPath = path.join(rootPath, fileName)
-        let stats = yield fs.stat(curPath)
-        if(stats.isDirectory()){
-            rmall(curPath)
-        }else{
-            yield fs.unlink(curPath)
+//let rmall = co.wrap(function* (rootPath){
+//    let fileNames = yield fs.readdir(rootPath)
+//
+//    for (let fileName of fileNames){
+//        let curPath = path.join(rootPath, fileName)
+//        let stats = yield fs.stat(curPath)
+//        if(stats.isDirectory()){
+//            rmall(curPath)
+//        }else{
+//            yield fs.unlink(curPath)
+//        }
+//    }
+//    yield fs.rmdir(rootPath)
+//})
+let rm = co.wrap(function* (inpath){
+    let stat = yield fs.stat(inpath);
+    if (stat.isDirectory()) {
+        let fileNames = yield fs.readdir(inpath);
+        for (let fileName of fileNames) {
+            let curPath = path.join(inpath, fileName);
+            yield rm(curPath);
         }
+        yield fs.rmdir(inpath)
+    } else {
+        yield fs.unlink(inpath)
     }
-    yield fs.rmdir(rootPath)
 })
-
-module.exports = rm
+module.exports = main
